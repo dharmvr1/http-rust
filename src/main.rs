2b26c5125be17add14c622ec35557d6d1c658a44
Dharm Singh <dharmhsing@gmail.com>
2025-08-13 01:09:34 +0530

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{BufRead, BufReader};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 use::std::{io::Write,net::TcpStream}; 
@@ -13,7 +14,7 @@ fn main() {
         match stream {
             Ok(stream) => {
                 // println!("accepted new connection");
-                handle_stream(stream);
+                handle_requset(stream);
                 println!("response send")
 
             }
@@ -27,8 +28,27 @@ fn main() {
 }
 
 
-fn handle_stream( mut stream :TcpStream){
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
-    stream.write_all(response.as_bytes()).unwrap();
+// fn handle_stream( mut stream :TcpStream){
+//     let response = "HTTP/1.1 200 OK\r\n\r\n";
+//     stream.write_all(response.as_bytes()).unwrap();
     
+// }
+
+fn handle_requset(mut stream :TcpStream){
+   
+   let reader = BufReader::new(&stream);
+   let request = reader.lines().next().unwrap().unwrap();
+
+   let status_line = match &request[..]{
+     "GET / HTTP/1.1" => "HTTP/1.1 200 OK",
+      _ => "HTTP/1.1 404 Not Found",
+   };
+
+   let response = format!("{status_line}\r\n\r\n");
+
+   match stream.write_all(response.as_bytes()) {
+    Ok(_)=>(),
+    Err(e)=>println!("could not send respond:{:?}",e)
+       
+   }
 }
